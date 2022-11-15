99e0f57551d55e815c6503a81af2ab34463cae64
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-11-15 19:28:22 -0300

    Using svg, creating several interfaces, creating components, contexts, autentication with google

--- nlw/copa/aulas/mobile/App.tsx
+++ nlw/copa/aulas/mobile/App.tsx
@@ -4,7 +4,7 @@ import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@ex
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
-import { SignIn } from './src/screens/Signin';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
@@ -20,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <SignIn/> : <Loading />}
+        {fontsLoaded ? <Pools/> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   )
--- nlw/copa/aulas/mobile/src/contexts/AuthContext.tsx
+++ nlw/copa/aulas/mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -11,20 +16,52 @@ interface AuthProviderProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps){
+    const [user, setUser] = useState<UserProps>({} as UserProps)
+    const [isUserLoading, setIsUserLoading] = useState(false);
+
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '214466390590-c3juinlrmje14b6526465dfjt70beaqt.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    })
 
     async function signIn(){
         console.log('Funfou');
+
+        try {
+            setIsUserLoading(true);
+            await promptAsync();
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+
+        } finally {
+            setIsUserLoading(false);
+        }
     }
+
+   async function signInWithGoogle(acess_token: string) {
+    console.log("TOKEN DE AUTENTICAÇÃO ===>", acess_token)
+   }
+
+    useEffect(() => {
+        if(response?.type === 'success' && response.authentication?.accessToken) {
+            signInWithGoogle(response.authentication.accessToken);
+        }
+    }, [response]);
 
     return(
         <AuthContext.Provider value={{
             signIn,
+            isUserLoading,
             user: {
                 name: 'Davi Kalel',
                 avatarUrl: 'https://github.com//dkat-davi.png'
