d16459a1350e847187173a22b71dc9e0ad89858c
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-11-16 19:11:11 -0300

    separating routes and creating googleId

--- nlw/copa/aulas/mobile/App.tsx
+++ nlw/copa/aulas/mobile/App.tsx
@@ -4,7 +4,7 @@ import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@ex
 
 import { AuthContextProvider } from './src/contexts/AuthContext';
 
-import { Pools } from './src/screens/Pools';
+import { SignIn } from './src/screens/Signin';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
@@ -20,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools/> : <Loading />}
+        {fontsLoaded ? <SignIn/> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   )
